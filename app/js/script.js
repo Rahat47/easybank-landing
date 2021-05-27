aa3cf2e771080947920c9478bc0d23f3347ca46b
Rahat47 <djrayhan8@gmail.com>
2021-05-27 23:00:18 +0600

    hamburger menu function added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+const menu = document.querySelector("#btnHamburger")
+const header = document.querySelector(".header")
+const overlay = document.querySelector(".overlay")
+
+menu.addEventListener("click", () => {
+
+    if (header.classList.contains("open")) {
+        header.classList.remove("open")
+        overlay.classList.remove("fade-in")
+        overlay.classList.add("fade-out")
+    } else {
+        header.classList.add("open")
+        overlay.classList.remove("fade-out")
+        overlay.classList.add("fade-in")
+    }
+
+})
