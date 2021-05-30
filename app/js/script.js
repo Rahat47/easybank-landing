08efaf23b0d9a94ec08dbf47a56e37742768572c
Rahat47 <djrayhan8@gmail.com>
2021-05-31 00:17:06 +0600

    mobile menu responsive completed, also menu close on button click added

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector("#btnHamburger")
 const header = document.querySelector(".header")
 const overlay = document.querySelector(".overlay")
+const mobileMenu = document.querySelector("#mobileMenu")
+const body = document.querySelector("body")
 
 menu.addEventListener("click", () => {
 
@@ -8,10 +10,24 @@ menu.addEventListener("click", () => {
         header.classList.remove("open")
         overlay.classList.remove("fade-in")
         overlay.classList.add("fade-out")
+        body.classList.remove("noScroll")
     } else {
         header.classList.add("open")
         overlay.classList.remove("fade-out")
         overlay.classList.add("fade-in")
+        body.classList.add('noScroll')
+
     }
 
+})
+
+
+//Close mobile menu on click
+mobileMenu.addEventListener('click', (e) => {
+    if (e.target.nodeName === 'A') {
+        header.classList.remove("open")
+        overlay.classList.remove("fade-in")
+        overlay.classList.add("fade-out")
+        body.classList.remove('noScroll')
+    }
 })
